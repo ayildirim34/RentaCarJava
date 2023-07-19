daf1f91d994a687d06991521cb9ffde2da283020
ayildirim34 <ayildirim3457@protonmail.com>
2023-07-19 18:35:30 +0200

    uc

---
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -0,0 +1,42 @@
+package kodlama.io.rentACar.business.concretes;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.request.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BrandManager implements BrandService {
+
+    private BrandRepository brandRepository;
+    @Autowired
+    public BrandManager(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+    @Override
+    public List<GetAllBrandsResponse> getAll() {
+        List <Brand> brands = brandRepository.findAll();
+        List <GetAllBrandsResponse> brandsResponse= new ArrayList<GetAllBrandsResponse>();
+        for (Brand brand: brands) {
+           GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
+           responseItem.setName(brand.getName());
+           responseItem.setId(brand.getId());
+
+           brandsResponse.add(responseItem);
+        }
+        return brandsResponse;
+    }
+
+    @Override
+    public void add(CreateBrandRequest createBrandRequest) {
+        Brand brand = new Brand();
+        brand.setName(createBrandRequest.getName());
+       this.brandRepository.save(brand);
+    }
+}
