ff91866ca7edffa90d1fade934b3ce7ecbfa338a
ayildirim34 <ayildirim3457@protonmail.com>
2023-07-23 11:54:49 +0200

    5. Woche erledigt

--- src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -1,12 +1,17 @@
 package kodlama.io.rentACar.business.abstracts;
 
 import kodlama.io.rentACar.business.request.CreateBrandRequest;
+import kodlama.io.rentACar.business.request.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 
 import java.util.List;
 
 public interface BrandService {
     List <GetAllBrandsResponse> getAll ();
+    GetByIdBrandResponse getById (int id);
     void add(CreateBrandRequest createBrandRequest);
+    void delete (int id);
+
+    void update(UpdateBrandRequest deleteBrandRequest);
 }
--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,27 +2,47 @@ package kodlama.io.rentACar.webApi.controllers;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.request.CreateBrandRequest;
+import kodlama.io.rentACar.business.request.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping ("/api/brands")
+@AllArgsConstructor
+
 public class BrandsController {
     private BrandService brandService;
 
-    public BrandsController(BrandService brandService) {
-        this.brandService = brandService;
-    }
-
-    @GetMapping ("/getall")
+    @GetMapping ()
     public List <GetAllBrandsResponse> getAll() {
         return brandService.getAll();
     }
 
-    @PostMapping ("/add")
+    @PostMapping ()
+    @ResponseStatus (code= HttpStatus.CREATED)
     public void add(@RequestBody CreateBrandRequest createBrandRequest) {
         this.brandService.add(createBrandRequest);
     }
+
+    @GetMapping ("/{id}")
+    public GetByIdBrandResponse getById(int id) {
+        return  brandService.getById(id);
+    }
+
+    @PutMapping()
+    public void update(@RequestBody UpdateBrandRequest updateBrandRequest) {
+        brandService.update(updateBrandRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(int id) {
+        brandService.delete(id);
+    }
+
+
 }
